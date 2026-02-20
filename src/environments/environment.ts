e7c0857823cbc9075290359da2b3a0c9f30f53fb
Francisco <francisco_r26@outlook.com>
2026-02-20 15:31:24 -0600

    added comments for authorization values

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
-  adminKeyHash: 'ee26300ba7ca4d6842b004eea31c8fa1c9d7d35db7654451f000d25669bb2b2c',
+  adminKeyHash: 'ee26300ba7ca4d6842b004eea31c8fa1c9d7d35db7654451f000d25669bb2b2c', //Generate it based on your "auth" data
   apiUrl: 'https://localhost:7009',
 
   auth: {
-    clientId: 'admin',
-    clientSecret: 'admin_2426',
+    clientId: 'admin', //change it for your clientId
+    clientSecret: 'admin_2426', //change it for your clientId
   },
 };
