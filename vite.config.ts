f008b65de8b2c6e629ba6e05e7e18c9f7e0977e9
Francisco <francisco_r26@outlook.com>
2026-05-19 17:11:41 -0600

    update coverage provider

--- vite.config.ts
+++ vite.config.ts
@@ -2,20 +2,10 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    globals: true,
-    environment: 'happy-dom',
-    setupFiles: ['src/test-setup.ts'],
-    include: ['src/**/*.spec.ts'],
-    reporters: ['default', 'junit'],
-    outputFile: {
-      junit: 'coverage/junit/TESTS-results.xml',
-    },
     coverage: {
-      provider: 'v8',
-      reporter: ['text', 'html', 'lcov'],
+      provider: 'istanbul',
+      reporter: ['html', 'lcov', 'text'],
       reportsDirectory: 'coverage',
-      include: ['src/**/*.ts'],
-      exclude: ['src/**/*.spec.ts', 'src/main.ts', 'src/test-setup.ts'],
     },
   },
 });
